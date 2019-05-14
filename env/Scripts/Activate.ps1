ce8d69dc07809f21c7a0d010ef528adc6551ecb8
Ron Wexler <ronmada@s.braude.ac.il>
2019-05-14 16:51:20 +0300

    commit

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Desktop\braudeAPI\env"
+$env:VIRTUAL_ENV="C:\Users\ronma\Desktop\braudeAPI\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
