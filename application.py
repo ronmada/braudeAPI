f26725b04c2cc85b5ac2e398bbc96513547c3022
sagi arieli <sagiarieli@gmail.com>
2019-05-30 22:13:19 +0300

    cdfvsxdcfx

--- application.py
+++ application.py
@@ -209,37 +209,36 @@ class Start_GA(Resource):
             specific_days_off = args['specific_days_off'][0].split(' ')
             args['specific_days_off'] = specific_days_off
             print(args['specific_days_off'])
-        ret = []
-        solutions = run(args['courses'],args['cluster'],args['specific_windows'],args['specific_days_off'],args['lecturer'])
-        for sol in solutions:
-            classes = []
-            for clas in sol.lectures:
-                lectures = []
-                for lecture in clas.lectures:
-                    lect = {
-                        "Semester": lecture.semester,
-                        "Day": lecture.day_in_week,
-                        "Start_time": lecture.start_time.hour,
-                        "End_time": lecture.end_time.hour,
-                        "Lecturer_name": lecture.lecturer,
-                        "Class_location": lecture.location
-                    }
-                    lectures.append(lect)
-                kita ={
-                    "Class_type": clas.type,
-                    "group_number": clas.g_number,
-                    "Related_groups": clas.related_groups,
-                    "lectures": lectures,
-                    "c_ID": clas.c_id,
-                    "lecturer": clas.lecturer
-                }
-                classes.append(kita)
-            solution = {
-                "classes": classes,
-                "Score" : sol.objective.string_fitness_paramenters()
 
+        sol = run(args['courses'],args['cluster'],args['specific_windows'],args['specific_days_off'],args['lecturer'])
+        classes = []
+        for clas in sol.lectures:
+            lectures = []
+            for lecture in clas.lectures:
+                lect = {
+                    "Semester": lecture.semester,
+                    "Day": lecture.day_in_week,
+                    "Start_time": lecture.start_time.hour,
+                    "End_time": lecture.end_time.hour,
+                    "Lecturer_name": lecture.lecturer,
+                    "Class_location": lecture.location
+                }
+                lectures.append(lect)
+            kita ={
+                "Class_type": clas.type,
+                "group_number": clas.g_number,
+                "Related_groups": clas.related_groups,
+                "lectures": lectures,
+                "c_ID": clas.c_id,
+                "lecturer": clas.lecturer
             }
-            ret.append(jsonify(solution))
+            classes.append(kita)
+        solution = {
+            "classes": classes,
+            "Score" : sol.objective.string_fitness_paramenters()
+
+        }
+        ret = jsonify(solution)
 
         return ret
 
