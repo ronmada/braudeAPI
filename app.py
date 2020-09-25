a30525668e7754dbfa418771f62f4054718e4228
Ron Wexler <ronmada@gmail.com>
2020-09-25 21:56:51 +0300

    h

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ api = Api(app)
 
 
 parser = reqparse.RequestParser()
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
